fdf43f4d6648b569b7d20b55c913f4b30304e2b6
samiksha2644 <samiksha.mote24@pccoepune.org>
2026-05-22 11:49:35 +0530

    frontend UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-gray-950 to-gray-900">
 
-      {/* Backend Test Button */}
+      {/* This is a Backend Test Button */}
       <div className="flex justify-center pt-6">
         <button
           onClick={testBackend}
